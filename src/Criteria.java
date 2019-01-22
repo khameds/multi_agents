d8a38538fa02d892a1055be81d63122fc2be9453
KHAMED Smaïl <khameds@gmail.com>
2019-01-22 22:18:33 +0100

    Define Criteria class

---
+++ src/Criteria.java
@@ -0,0 +1,98 @@
+public class Criteria
+{
+    private int a1,a2,a3,a4;
+    private int b1,b2,b3,b4;
+
+    public Criteria(int a1, int a2, int a3, int a4, int b1, int b2, int b3, int b4)
+    {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+        this.a4 = a4;
+        this.b1 = b1;
+        this.b2 = b2;
+        this.b3 = b3;
+        this.b4 = b4;
+    }
+
+    /*** Getters and Setters ***/
+    public int getA1()
+    {
+        return a1;
+    }
+
+    public void setA1(int a1)
+    {
+        this.a1 = a1;
+    }
+
+    public int getA2()
+    {
+        return a2;
+    }
+
+    public void setA2(int a2)
+    {
+        this.a2 = a2;
+    }
+
+    public int getA3()
+    {
+        return a3;
+    }
+
+    public void setA3(int a3)
+    {
+        this.a3 = a3;
+    }
+
+    public int getA4()
+    {
+        return a4;
+    }
+
+    public void setA4(int a4)
+    {
+        this.a4 = a4;
+    }
+
+    public int getB1()
+    {
+        return b1;
+    }
+
+    public void setB1(int b1)
+    {
+        this.b1 = b1;
+    }
+
+    public int getB2()
+    {
+        return b2;
+    }
+
+    public void setB2(int b2)
+    {
+        this.b2 = b2;
+    }
+
+    public int getB3()
+    {
+        return b3;
+    }
+
+    public void setB3(int b3)
+    {
+        this.b3 = b3;
+    }
+
+    public int getB4()
+    {
+        return b4;
+    }
+
+    public void setB4(int b4)
+    {
+        this.b4 = b4;
+    }
+}
