bc8d7b6db77e1a458ef9ae2c572e7eb654b891bb
KHAMED Smaïl <khameds@gmail.com>
2019-01-22 20:32:01 +0100

    Define FileLoader class

---
+++ src/input/scen01/FileLoader.java
@@ -0,0 +1,24 @@
+package input.scen01;
+
+public class FileLoader
+{
+    public  String getCstFile()
+    {
+        return getClass().getResource("cst.txt").getPath();
+    }
+
+    public  String getCtrFile()
+    {
+        return getClass().getResource("ctr.txt").getPath();
+    }
+
+    public  String getDomFile()
+    {
+        return getClass().getResource("dom.txt").getPath();
+    }
+
+    public  String getVarFile()
+    {
+        return getClass().getResource("var.txt").getPath();
+    }
+}
