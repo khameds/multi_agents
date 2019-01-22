e6673b3f2f54a1633e1d8c38dc6e5dc2850bdf87
KHAMED Smaïl <khameds@gmail.com>
2019-01-22 20:33:37 +0100

    Define Main class

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Parser parser = new Parser();
+
+        parser.readInputs();
+    }
+}
+
