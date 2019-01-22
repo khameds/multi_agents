9682eb1e6e863a766d62aa5b86de5b4b6bcc03a0
KHAMED Smaïl <khameds@gmail.com>
2019-01-22 17:23:09 +0100

    Define Frequency

---
+++ src/Frequency.java
@@ -0,0 +1,38 @@
+public class Frequency
+{
+    private int id;
+    private int agentId;
+    private int domain;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public int getAgentId()
+    {
+        return agentId;
+    }
+
+    public void setAgentId(int agentId)
+    {
+        this.agentId = agentId;
+    }
+
+    public int getDomain()
+    {
+        return domain;
+    }
+
+    public void setDomain(int domain)
+    {
+        this.domain = domain;
+    }
+
+
+}
