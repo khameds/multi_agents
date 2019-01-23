d5a8225df2600162b3650211a4cae11e842d8f88
KHAMED Smaïl <khameds@gmail.com>
2019-01-23 17:41:19 +0100

    Add Agent parameters

--- src/Agent.java
+++ src/Agent.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Agent
 {
+    private static int agentId = 0;
 
+    {
+        agentId++;
+    }
+
+    private List<Integer> frequencyIds;
+
+    public Agent()
+    {
+        frequencyIds = new ArrayList<>();
+    }
+
+    public void addFrequency(Integer frequency)
+    {
+        frequencyIds.add(frequency);
+    }
+
+
+    /*** Getters and Setters ***/
+
+    public static int getAgentId()
+    {
+        return agentId;
+    }
+
+    public List<Integer> getFrequencyIds()
+    {
+        return frequencyIds;
+    }
+
+    public void setFrequencyIds(List<Integer> frequencyIds)
+    {
+        this.frequencyIds = frequencyIds;
+    }
 }
